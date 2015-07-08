e7fd03e1ee1f3ba8e10d30750478333a92ddccc9
Vivek Sachdeva <vivek.sachdeva@intelligrape.com>
2015-07-08 16:55:53 +0530

    sling job files

---
+++ bundle/src/main/java/com/tothenew/impl/MyJobConsumer.java
@@ -0,0 +1,25 @@
+package com.tothenew.impl;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.event.jobs.Job;
+import org.apache.sling.event.jobs.consumer.JobConsumer;
+
+@Component
+@Service(value={JobConsumer.class})
+@Property(name=JobConsumer.PROPERTY_TOPICS, value="my/special/jobtopic")
+public class MyJobConsumer implements JobConsumer {
+
+    public JobResult process(final Job job) {
+        System.out.println("==========================================");
+        System.out.println(job.getProperty("count"));
+        System.out.println("==========================================");
+        // process the job and return the result
+        int count = (Integer)job.getProperty("count");
+        if(count > 3){
+           return JobResult.FAILED;
+        }
+        return JobResult.OK;
+    }
+}
---
+++ bundle/src/main/java/com/tothenew/impl/MyOsgiEventHandler.java
@@ -0,0 +1,22 @@
+package com.tothenew.impl;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+
+/**
+ * Created by intelligrape on 8/7/15.
+ */
+@Service
+@Component(enabled = true, immediate = true)
+@Property(name=org.osgi.service.event.EventConstants.EVENT_TOPIC,
+        value=org.apache.sling.api.SlingConstants.TOPIC_RESOURCE_ADDED)
+public class MyOsgiEventHandler implements EventHandler {
+
+    @Override
+    public void handleEvent(Event event) {
+        System.out.print("My OSGi Event Handler");
+    }
+}
